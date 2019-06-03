d742e2688f45d5387e8db1c12c22a3a08f2cdb54
ELİF UÇAR <48561599+ELFUCAR@users.noreply.github.com>
2019-06-03 18:08:11 +0300

    fibonacci sequence1

---
+++ python-fibonacci-sequence.py
@@ -0,0 +1,26 @@
+'''
+Python fibonacci sequence
+
+'''
+def fibo(n)
+    a = 10
+
+    n1 = 1
+    n2 = 1
+    
+    
+    count = 0
+
+    if a == 1:
+      print("first 10 numbers:")
+      print(n1)
+    else:
+        print("first 10 numbers:")
+        while count < a:
+       
+            print(n1)
+            nsum = n1 + n2
+      
+            n1 = n2
+            n2 = nsum
+            count += 1
